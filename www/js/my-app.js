1044d3652d064c69a24d77d405c70ed1c9fdad03
niveksngry <47378047+niveksngry@users.noreply.github.com>
2019-02-07 10:43:28 +0800

    Cleaned up folders

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -21,27 +21,27 @@ var app = new Framework7({
     },
     {
       path: '/navi/',
-      url: 'navi.html',
+      url: '/pages/navi.html',
     },
     {
       path: '/wedding_ceremony/',
-      url: 'wedding_ceremony.html',
+      url: '/pages/wedding_ceremony.html',
     },
     {
       path: '/wedding_reception/',
-      url: 'wedding_reception.html',
+      url: '/pages/wedding_reception.html',
     },
     {
       path: '/date_location/',
-      url: 'date_location.html',
+      url: '/pages/date_location.html',
     },
     {
       path: '/entourage/',
-      url: 'entourage.html',
+      url: '/pages/entourage.html',
     },
     {
       path: '/food_menu/',
-      url: 'food_menu.html',
+      url: '/pages/food_menu.html',
     },
   ],
   // ... other parameters
